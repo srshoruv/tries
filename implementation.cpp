ac54e04d79932b4f29236e4b9d333edbf1c8101a
srshoruv <srshoruv2002@gmail.com>
2024-10-01 04:10:26 +0600

    implemented search function

--- implementation.cpp
+++ implementation.cpp
@@ -32,6 +32,20 @@ public:
         }
         temp->endOfWord = true;
     }
+
+    bool search(string key) {
+        Node* temp = root;
+
+        for (int i=0; i<key.size(); i++) {
+            if (temp->children.count(key[i])) {
+                temp = temp->children[key[i]];
+            } else {
+                return false;
+            }
+        }
+
+        return temp->endOfWord;
+    }
 };
 
 int main (){
@@ -41,4 +55,6 @@ int main (){
     for (int i=0; i<words.size(); i++) {
         trie.insert(words[i]);
     }
+
+    cout << trie.search("ant");
 }
